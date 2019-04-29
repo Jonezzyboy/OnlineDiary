255c9f55094f3dc2d69f4668c9446ff1ab03b9cb
Jonezzyboy <jonezzyboy@gmail.com>
2019-04-29 19:39:10 +0100

    Updated Appointment
    Fixed bug where appointments couldn't be made on the current date

--- src/main/java/alan/OnlineDiary/bus/AppointmentService.java
+++ src/main/java/alan/OnlineDiary/bus/AppointmentService.java
@@ -9,7 +9,11 @@ import alan.OnlineDiary.ents.Appointment;
 import alan.OnlineDiary.ents.User;
 import alan.OnlineDiary.pers.AppointmentFacade;
 import alan.OnlineDiary.pers.UserFacade;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -47,11 +51,9 @@ public class AppointmentService {
                 af.create(a);
                 return true;
             } else {
-                FacesContext.getCurrentInstance().addMessage("appointForm:userList", new FacesMessage("There is an appointment time clash"));
                 return false;
             }
         } else {
-            FacesContext.getCurrentInstance().addMessage("appointForm:startTime", new FacesMessage("Start Time must be before End Time"));
             return false;
         }
 
@@ -65,10 +67,12 @@ public class AppointmentService {
             userApps = user.getAppointments();
             if (!appointmentClash) {
                 for (Appointment app : userApps) {
-                    if (a.getStartDate().equals(app.getStartDate()) &&
-                            (a.getStartTime().after(app.getStartTime()) || a.getStartTime().compareTo(app.getStartTime()) == 0)
+                    if (a.getStartDate().equals(app.getStartDate())
+                            && (a.getStartTime().after(app.getStartTime()) || a.getStartTime().compareTo(app.getStartTime()) == 0)
                             && (a.getEndTime().before(app.getEndTime()) || a.getEndTime().compareTo(app.getEndTime()) == 0)) {
                         appointmentClash = true;
+                        FacesContext.getCurrentInstance().addMessage("appointForm:userList", new FacesMessage("There is an appointment time clash"));
+                        return appointmentClash;
                     }
                 }
             }
@@ -78,9 +82,30 @@ public class AppointmentService {
 
     public Boolean checkTimes(Appointment a) {
         Boolean timeValid = true;
-        if (a.getStartTime().after(a.getEndTime()) || a.getStartTime().compareTo(a.getEndTime()) == 0) {
+        Date currentDate = new Date();
+        Date appStartDate = joinStartTimes(a.getStartTime());
+        if (appStartDate.before(currentDate)) {
             timeValid = false;
+            FacesContext.getCurrentInstance().addMessage("appointForm:startDate", new FacesMessage("Start Date must not be set in the past"));
+            if (a.getStartTime().after(a.getEndTime()) || a.getStartTime().compareTo(a.getEndTime()) == 0) {
+                FacesContext.getCurrentInstance().addMessage("appointForm:startTime", new FacesMessage("Start Time must be before End Time"));
+            }
         }
         return timeValid;
+    }
+    
+    public Date joinStartTimes(Date d){
+        // Joins the start date and start time
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        String times = dateFormat.format(d);
+        String[] hourMin = times.split(":");
+        int hour = Integer.parseInt(hourMin[0]);
+        int mins = Integer.parseInt(hourMin[1]);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(d);
+        cal.set(Calendar.HOUR, hour);
+        cal.set(Calendar.MINUTE, mins);
+        Date appStartDate = cal.getTime();
+        return appStartDate;
     }
 }
